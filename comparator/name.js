6b751ec89cac8a396aad146237c13fd4dfec99d1
Sebastian <sebastian.ovide@gmail.com>
2018-10-18 15:14:00 +0100

    removed console log

--- comparator/name.js
+++ comparator/name.js
@@ -1,7 +1,6 @@
 //module to compare names of two road
 const stringSimilarity = require('string-similarity');
 exports.compare = (nameOne, nameTwo) => {
-  console.log("XXXXXX ", stringSimilarity.compareTwoStrings(nameOne, nameTwo))
   const similarRtn = stringSimilarity.compareTwoStrings(nameOne, nameTwo) > 0.6;
   return similarRtn && !!nameOne ;
 }
